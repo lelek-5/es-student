d0d03959a28f78bc965737fd029a33d7572d90a7
Алексей Пасечник <pasechnik74@mail.ru>
2026-09-25 22:59:24 +0300

    сделал 1,3,5

--- 134-led-module/led/led.c
+++ 134-led-module/led/led.c
@@ -2,27 +2,23 @@
 #include "hardware/gpio.h"
 
 const uint LED_PIN = 25;
-
 static bool led_state = false;
 
-void led_init(void)
-{
-    // инициализируем пин светодиода и настраиваем его на выход
+void led_init(void) {
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
     led_set(false);
 }
 
-void led_set(bool on)
-{
+void led_set(bool on) {
     led_state = on;
     gpio_put(LED_PIN, led_state);
 }
 
-void led_toggle(void)
-{
+void led_toggle(void) {
     led_set(!led_state);
 }
 
-bool led_is_on(void)
-{
+bool led_is_on(void) {
     return led_state;
 }
