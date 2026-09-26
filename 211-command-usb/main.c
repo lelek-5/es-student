7145f451a7623ab418eefaf1c5d32bc4469e7305
Алексей Пасечник <pasechnik74@mail.ru>
2026-09-26 10:52:17 +0300

    Сделана п 2.1.2

--- 211-command-usb/main.c
+++ 211-command-usb/main.c
@@ -11,26 +11,62 @@ const uint BUTTON_PIN = 15;
 char line[LINE_SIZE];
 uint line_length = 0;
 
+typedef void (*command_handler_t)(void);
+
+void cmd_enable(void) {
+    led_set(true);
+    LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+}
+
+void cmd_disable(void) {
+    led_set(false);
+    LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+}
+
+void cmd_info(void) {
+    device_info();
+}
+
+void cmd_version(void) {
+    log_version();
+}
+
+void cmd_ping(void) {
+    printf("pong\n");
+}
+
+struct command_t {
+    const char *name;
+    command_handler_t handler;
+};
+
+const struct command_t commands[] = {
+    { "enable", cmd_enable },
+    { "disable", cmd_disable },
+    { "info", cmd_info },
+    { "version", cmd_version },
+    { "ping", cmd_ping },
+};
+
+#define COMMAND_COUNT (sizeof(commands) / sizeof(commands[0]))
+
+void handle_command(const char *command) {
+    for (uint i = 0; i < COMMAND_COUNT; i++) {
+        if (strcmp(command, commands[i].name) == 0) {
+            if (commands[i].handler != NULL) {
+                commands[i].handler();
+            }
+            return;
+        }
+    }
+
+    LOG_ERR("unknown command: %s\n", command);
+}
+
 bool get_button_debounce(uint pin) {
     bool state = gpio_get(pin);
     sleep_ms(20);
     return state;
-}
-
-void handle_command(const char *command) {
-    if (strcmp(command, "enable") == 0) {
-        led_set(true);
-        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
-    } else if (strcmp(command, "disable") == 0) {
-        led_set(false);
-        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
-    } else if (strcmp(command, "info") == 0) {
-        device_info();
-    } else if (strcmp(command, "version") == 0) {
-        log_version();
-    } else {
-        LOG_ERR("unknown command: %s\n", command);
-    }
 }
 
 void read_line(void) {
